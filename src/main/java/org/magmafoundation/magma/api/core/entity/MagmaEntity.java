fd3b89964dee992cbc53c0bd443809354bcad041
Hexeption <minecraftfun201@gmail.com>
2019-11-24 20:57:05 +0000

    Add Missing File

--- src/main/java/org/magmafoundation/magma/api/core/entity/MagmaEntity.java
+++ src/main/java/org/magmafoundation/magma/api/core/entity/MagmaEntity.java
@@ -1,11 +1,499 @@
 package org.magmafoundation.magma.api.core.entity;
 
+import com.google.common.base.Preconditions;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.projectile.AbstractArrowEntity;
+import org.bukkit.EntityEffect;
+import org.bukkit.Location;
+import org.bukkit.Server;
+import org.bukkit.World;
+import org.bukkit.block.BlockFace;
+import org.bukkit.block.PistonMoveReaction;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Pose;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.player.PlayerTeleportEvent.TeleportCause;
+import org.bukkit.metadata.MetadataValue;
+import org.bukkit.permissions.Permission;
+import org.bukkit.permissions.PermissionAttachment;
+import org.bukkit.permissions.PermissionAttachmentInfo;
+import org.bukkit.persistence.PersistentDataContainer;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.util.BoundingBox;
+import org.bukkit.util.Vector;
+import org.magmafoundation.magma.api.bridge.IBridgeEntity;
+import org.magmafoundation.magma.api.core.MagmaServer;
+
 /**
  * MagmaEntity
  *
  * @author Hexeption admin@hexeption.co.uk
  * @since 24/11/2019 - 08:12 pm
  */
-public class MagmaEntity {
+public class MagmaEntity implements org.bukkit.entity.Entity {
 
+    protected final MagmaServer server;
+    protected Entity entity;
+
+    public MagmaEntity(MagmaServer server, Entity entity) {
+        this.server = server;
+        this.entity = entity;
+    }
+
+    public static MagmaEntity getEntity(MagmaServer server, Entity entity) {
+        return null;
+    }
+
+    @Override
+    public Location getLocation() {
+        return new Location(getWorld(), entity.posX, entity.posY, entity.posZ,
+            ((IBridgeEntity) entity).getBukkitYaw(), entity.rotationPitch);
+    }
+
+    @Override
+    public Location getLocation(Location loc) {
+        if (loc != null) {
+            loc.setWorld(getWorld());
+            loc.setX(entity.posX);
+            loc.setY(entity.posY);
+            loc.setZ(entity.posZ);
+            loc.setYaw(((IBridgeEntity) entity).getBukkitYaw());
+            loc.setPitch(entity.rotationPitch);
+        }
+        return loc;
+    }
+
+    @Override
+    public void setVelocity(Vector velocity) {
+        Preconditions.checkArgument(velocity != null, "velocity");
+        velocity.checkFinite();
+        entity.setMotion(velocity.getX(), velocity.getY(), velocity.getZ());
+    }
+
+    @Override
+    public Vector getVelocity() {
+        return new Vector(entity.getMotion().x, entity.getMotion().y, entity.getMotion().z);
+    }
+
+    @Override
+    public double getHeight() {
+        return getEntity().getHeight();
+    }
+
+    @Override
+    public double getWidth() {
+        return getEntity().getWidth();
+    }
+
+    @Override
+    public BoundingBox getBoundingBox() {
+        return null;
+    }
+
+    @Override
+    public boolean isOnGround() {
+        if (entity instanceof AbstractArrowEntity) {
+            return ((AbstractArrowEntity) entity).onGround;
+        }
+        return entity.onGround;
+    }
+
+    @Override
+    public World getWorld() {
+        return null;
+    }
+
+    @Override
+    public void setRotation(float yaw, float pitch) {
+    }
+
+    @Override
+    public boolean teleport(Location location) {
+        Preconditions.checkArgument(location != null, "location");
+        location.checkFinite();
+
+        if (entity.isBeingRidden()) {
+
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean teleport(Location location, TeleportCause cause) {
+        return false;
+    }
+
+    @Override
+    public boolean teleport(org.bukkit.entity.Entity destination) {
+        return false;
+    }
+
+    @Override
+    public boolean teleport(org.bukkit.entity.Entity destination, TeleportCause cause) {
+        return false;
+    }
+
+    @Override
+    public List<org.bukkit.entity.Entity> getNearbyEntities(double x, double y, double z) {
+        return null;
+    }
+
+    @Override
+    public int getEntityId() {
+        return 0;
+    }
+
+    @Override
+    public int getFireTicks() {
+        return 0;
+    }
+
+    @Override
+    public int getMaxFireTicks() {
+        return 0;
+    }
+
+    @Override
+    public void setFireTicks(int ticks) {
+
+    }
+
+    @Override
+    public void remove() {
+
+    }
+
+    @Override
+    public boolean isDead() {
+        return false;
+    }
+
+    @Override
+    public boolean isValid() {
+        return false;
+    }
+
+    @Override
+    public void sendMessage(String message) {
+
+    }
+
+    @Override
+    public void sendMessage(String[] messages) {
+
+    }
+
+    @Override
+    public Server getServer() {
+        return null;
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public boolean isPersistent() {
+        return false;
+    }
+
+    @Override
+    public void setPersistent(boolean persistent) {
+
+    }
+
+    @Override
+    public org.bukkit.entity.Entity getPassenger() {
+        return null;
+    }
+
+    @Override
+    public boolean setPassenger(org.bukkit.entity.Entity passenger) {
+        return false;
+    }
+
+    @Override
+    public List<org.bukkit.entity.Entity> getPassengers() {
+        return null;
+    }
+
+    @Override
+    public boolean addPassenger(org.bukkit.entity.Entity passenger) {
+        return false;
+    }
+
+    @Override
+    public boolean removePassenger(org.bukkit.entity.Entity passenger) {
+        return false;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean eject() {
+        return false;
+    }
+
+    @Override
+    public float getFallDistance() {
+        return 0;
+    }
+
+    @Override
+    public void setFallDistance(float distance) {
+
+    }
+
+    @Override
+    public void setLastDamageCause(EntityDamageEvent event) {
+
+    }
+
+    @Override
+    public EntityDamageEvent getLastDamageCause() {
+        return null;
+    }
+
+    @Override
+    public UUID getUniqueId() {
+        return null;
+    }
+
+    @Override
+    public int getTicksLived() {
+        return 0;
+    }
+
+    @Override
+    public void setTicksLived(int value) {
+
+    }
+
+    @Override
+    public void playEffect(EntityEffect type) {
+
+    }
+
+    @Override
+    public EntityType getType() {
+        return null;
+    }
+
+    @Override
+    public boolean isInsideVehicle() {
+        return false;
+    }
+
+    @Override
+    public boolean leaveVehicle() {
+        return false;
+    }
+
+    @Override
+    public org.bukkit.entity.Entity getVehicle() {
+        return null;
+    }
+
+    @Override
+    public void setCustomNameVisible(boolean flag) {
+
+    }
+
+    @Override
+    public boolean isCustomNameVisible() {
+        return false;
+    }
+
+    @Override
+    public void setGlowing(boolean flag) {
+
+    }
+
+    @Override
+    public boolean isGlowing() {
+        return false;
+    }
+
+    @Override
+    public void setInvulnerable(boolean flag) {
+
+    }
+
+    @Override
+    public boolean isInvulnerable() {
+        return false;
+    }
+
+    @Override
+    public boolean isSilent() {
+        return false;
+    }
+
+    @Override
+    public void setSilent(boolean flag) {
+
+    }
+
+    @Override
+    public boolean hasGravity() {
+        return false;
+    }
+
+    @Override
+    public void setGravity(boolean gravity) {
+
+    }
+
+    @Override
+    public int getPortalCooldown() {
+        return 0;
+    }
+
+    @Override
+    public void setPortalCooldown(int cooldown) {
+
+    }
+
+    @Override
+    public Set<String> getScoreboardTags() {
+        return null;
+    }
+
+    @Override
+    public boolean addScoreboardTag(String tag) {
+        return false;
+    }
+
+    @Override
+    public boolean removeScoreboardTag(String tag) {
+        return false;
+    }
+
+    @Override
+    public PistonMoveReaction getPistonMoveReaction() {
+        return null;
+    }
+
+    @Override
+    public BlockFace getFacing() {
+        return null;
+    }
+
+    @Override
+    public Pose getPose() {
+        return null;
+    }
+
+    @Override
+    public String getCustomName() {
+        return null;
+    }
+
+    @Override
+    public void setCustomName(String name) {
+
+    }
+
+    @Override
+    public void setMetadata(String metadataKey, MetadataValue newMetadataValue) {
+
+    }
+
+    @Override
+    public List<MetadataValue> getMetadata(String metadataKey) {
+        return null;
+    }
+
+    @Override
+    public boolean hasMetadata(String metadataKey) {
+        return false;
+    }
+
+    @Override
+    public void removeMetadata(String metadataKey, Plugin owningPlugin) {
+
+    }
+
+    @Override
+    public boolean isPermissionSet(String name) {
+        return false;
+    }
+
+    @Override
+    public boolean isPermissionSet(Permission perm) {
+        return false;
+    }
+
+    @Override
+    public boolean hasPermission(String name) {
+        return false;
+    }
+
+    @Override
+    public boolean hasPermission(Permission perm) {
+        return false;
+    }
+
+    @Override
+    public PermissionAttachment addAttachment(Plugin plugin, String name, boolean value) {
+        return null;
+    }
+
+    @Override
+    public PermissionAttachment addAttachment(Plugin plugin) {
+        return null;
+    }
+
+    @Override
+    public PermissionAttachment addAttachment(Plugin plugin, String name, boolean value,
+        int ticks) {
+        return null;
+    }
+
+    @Override
+    public PermissionAttachment addAttachment(Plugin plugin, int ticks) {
+        return null;
+    }
+
+    @Override
+    public void removeAttachment(PermissionAttachment attachment) {
+
+    }
+
+    @Override
+    public void recalculatePermissions() {
+
+    }
+
+    @Override
+    public Set<PermissionAttachmentInfo> getEffectivePermissions() {
+        return null;
+    }
+
+    @Override
+    public boolean isOp() {
+        return false;
+    }
+
+    @Override
+    public void setOp(boolean value) {
+
+    }
+
+    @Override
+    public PersistentDataContainer getPersistentDataContainer() {
+        return null;
+    }
+
+    public Entity getEntity() {
+        return entity;
+    }
 }
