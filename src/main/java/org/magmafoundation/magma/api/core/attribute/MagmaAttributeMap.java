0026c4a5a3d120438fd80876aec713bcb083c3b5
KingdevNL <jasper@kingdev.nl>
2019-11-25 15:35:43 +0100

    Added AttributeInstance and AttributeMap

---
+++ src/main/java/org/magmafoundation/magma/api/core/attribute/MagmaAttributeMap.java
@@ -0,0 +1,58 @@
+package org.magmafoundation.magma.api.core.attribute;
+
+
+import com.google.common.base.CaseFormat;
+import com.google.common.base.Preconditions;
+import java.util.Locale;
+import  net.minecraft.entity.ai.attributes.AbstractAttributeMap;
+import net.minecraft.entity.ai.attributes.IAttributeInstance;
+import org.apache.commons.lang3.EnumUtils;
+import org.bukkit.attribute.Attributable;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeInstance;
+
+public class MagmaAttributeMap implements Attributable {
+
+    private final AbstractAttributeMap handle;
+
+    public MagmaAttributeMap(AbstractAttributeMap handle) {
+        this.handle = handle;
+    }
+
+    @Override
+    public AttributeInstance getAttribute(Attribute attribute) {
+        Preconditions.checkArgument(attribute != null, "attribute");
+
+       IAttributeInstance nms = handle.getAttributeInstanceByName(toMinecraft(attribute.name()));
+
+        return (nms == null) ? null : new MagmaAttributeInstance(nms, attribute);
+    }
+
+    public static String toMinecraft(String bukkit) {
+        int first = bukkit.indexOf('_');
+        int second = bukkit.indexOf('_', first + 1);
+
+        StringBuilder sb = new StringBuilder(bukkit.toLowerCase(java.util.Locale.ENGLISH));
+
+        sb.setCharAt(first, '.');
+        if (second != -1) {
+            sb.deleteCharAt(second);
+            sb.setCharAt(second, bukkit.charAt(second + 1));
+        }
+
+        return sb.toString();
+    }
+
+    public static String toMinecraft(Attribute attribute) {
+        return toMinecraft(attribute.name());
+    }
+
+    public static Attribute fromMinecraft(String nms) {
+        String[] split = nms.split("\\.", 2);
+
+        String generic = split[0];
+        String descriptor = CaseFormat.LOWER_CAMEL.to(CaseFormat.UPPER_UNDERSCORE, split[1]); // movementSpeed -> MOVEMENT_SPEED
+        String fin = generic + "_" + descriptor;
+        return EnumUtils.getEnum(Attribute.class, fin.toUpperCase(Locale.ROOT)); // so we can return null without throwing exceptions
+    }
+}
