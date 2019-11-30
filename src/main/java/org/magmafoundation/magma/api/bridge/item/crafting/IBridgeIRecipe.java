e8d4be66546662f0e6cbde4cf14935b7ad39586a
Hexeption <minecraftfun201@gmail.com>
2019-11-30 07:48:30 +0000

    More work on inventory and crafting mixins

---
+++ src/main/java/org/magmafoundation/magma/api/bridge/item/crafting/IBridgeIRecipe.java
@@ -0,0 +1,14 @@
+package org.magmafoundation.magma.api.bridge.item.crafting;
+
+import org.bukkit.inventory.Recipe;
+
+/**
+ * IBridgeIRecipe
+ *
+ * @author Hexeption admin@hexeption.co.uk
+ * @since 30/11/2019 - 07:38 am
+ */
+public interface IBridgeIRecipe {
+
+    Recipe toBukkitRecipe();
+}
--- src/main/java/org/magmafoundation/magma/api/mixin/inventory/container/MixinPlayerContainer.java
+++ src/main/java/org/magmafoundation/magma/api/mixin/inventory/container/MixinPlayerContainer.java
@@ -5,8 +5,13 @@ import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.inventory.CraftResultInventory;
 import net.minecraft.inventory.CraftingInventory;
 import net.minecraft.inventory.container.PlayerContainer;
+import org.bukkit.entity.HumanEntity;
+import org.bukkit.inventory.InventoryView;
+import org.magmafoundation.magma.api.bridge.entity.IBridgeEntity;
 import org.magmafoundation.magma.api.bridge.inventory.IBridgeCraftingInventorty;
 import org.magmafoundation.magma.api.bridge.inventory.container.IBridgePlayerContainer;
+import org.magmafoundation.magma.api.core.inventory.MagmaInventoryCrafting;
+import org.magmafoundation.magma.api.core.inventory.MagmaInventoryView;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Mutable;
@@ -22,10 +27,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
  * @since 27/11/2019 - 06:52 am
  */
 @Mixin(PlayerContainer.class)
-public class MixinPlayerContainer implements IBridgePlayerContainer {
+public class MixinPlayerContainer extends MixinContainer implements IBridgePlayerContainer {
 
-
-    //    private MagmaInventoryView bukkitEntity = null;
+    private MagmaInventoryView bukkitEntity = null;
     private PlayerInventory playerInventory;
 
     //@formatter:off
@@ -33,6 +37,9 @@ public class MixinPlayerContainer implements IBridgePlayerContainer {
     @Mutable @Shadow @Final private CraftResultInventory field_75179_f;
     //@formatter:on
 
+    @Shadow
+    @Final
+    private PlayerEntity player;
 
     @Inject(method = "<init>", at = @At("HEAD"))
     private void init(PlayerInventory playerInventory, boolean localWorld, PlayerEntity playerIn,
@@ -48,5 +55,21 @@ public class MixinPlayerContainer implements IBridgePlayerContainer {
     @Override
     public PlayerInventory getPlayerInventory() {
         return playerInventory;
+    }
+
+    @Override
+    public InventoryView getBukkitView() {
+        if (bukkitEntity != null) {
+            return bukkitEntity;
+        }
+
+        MagmaInventoryCrafting inventoryCrafting = new MagmaInventoryCrafting(this.field_75181_e,
+            this.field_75179_f);
+        bukkitEntity = new MagmaInventoryView((PlayerContainer) (Object) this,
+            (HumanEntity) ((IBridgeEntity) this.playerInventory.player).getBukkitEntity(),
+            inventoryCrafting
+        ); // TODO: 30/11/2019 Add IBridgeHumanEntity
+
+        return bukkitEntity;
     }
 }
