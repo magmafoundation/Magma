0e43213a75f1b371f61d0f948a86179cdce677d0
Hexeption <minecraftfun201@gmail.com>
2020-04-17 10:28:39 +0100

    Added a config options for debug banner patterns

--- src/main/java/org/magmafoundation/magma/configuration/MagmaConfig.java
+++ src/main/java/org/magmafoundation/magma/configuration/MagmaConfig.java
@@ -37,6 +37,7 @@ public class MagmaConfig extends ConfigBase {
 
     //============================Debug======================================
     public final BooleanValue debugPrintBukkitMatterials = new BooleanValue(this, "debug.debugPrintBukkitMatterials", false, "Prints the Forge Bukkit Materials");
+    public final BooleanValue debugPrintBukkitBannerPatterns = new BooleanValue(this, "debug.debugPrintBukkitBannerPatterns", false, "Prints the Forge Bukkit Banner Patterns");
     public final BooleanValue debugPrintCommandNode = new BooleanValue(this, "debug.debugPrintCommandNode", false, "Prints out all Command Nodes for permissions");
     //============================Debug======================================
 
