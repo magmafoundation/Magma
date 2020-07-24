127df417d032795deb6aa3ddfb9336b0994f7399
Kwright02 <kyle.wright02@gmail.com>
2020-07-23 21:15:20 -0500

    Fixed reduntant config options

--- src/main/java/org/magmafoundation/magma/configuration/MagmaConfig.java
+++ src/main/java/org/magmafoundation/magma/configuration/MagmaConfig.java
@@ -48,10 +48,6 @@ public class MagmaConfig extends ConfigBase {
     public final BooleanValue overrideServerBrand = new BooleanValue(this, "magma.advanced.override-brand", false, "Enables overriding the brand string");
     public final StringValue serverBrand = new StringValue(this, "magma.advanced.override-brand-name", "Spigot", "Value to use for new brand string");
 
-
-    public final BooleanValue overrideServerBrand = new BooleanValue(this, "magma.advanced.override-brand", false, "Enables or disables overriding the server brand");
-    public final StringValue serverBrandValue = new StringValue(this, "magma.advanced.override-value", "Spigot", "Value to use for server brand when overriding is enabled");
-
     private final String HEADER = "This is the main configuration file for Magma.\n" +
         "\n" +
         "Site: https://magmafoundation.org\n" +
